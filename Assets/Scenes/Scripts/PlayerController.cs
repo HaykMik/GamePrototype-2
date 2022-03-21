c4243082650a405dc3b290f2591606646e92e8e3
Lutas <Fanateshen@gmail.com>
2022-03-21 22:14:49 +0300

    Added MoveForward.cs

--- Assets/Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -6,9 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 20f;
+    public float xRange = 10f;
 
     void Update()
     {
+        // Keep the player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
     }
