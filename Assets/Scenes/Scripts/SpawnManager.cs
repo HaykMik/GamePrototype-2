03b577c2f8ec11b5c68addd7ce355e8308558a92
Lutas <Fanateshen@gmail.com>
2022-04-02 10:56:50 +0300

    Update SpawnManager.cs

--- Assets/Scenes/Scripts/SpawnManager.cs
+++ Assets/Scenes/Scripts/SpawnManager.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
-    void Start()
-    {
-        
-    }
+    private float spawnRangeX = 20f;
+    private float spawnRangeZ = 20f;
 
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.S))
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
+            Instantiate(animalPrefabs[animalIndex], spawnPos,
                 animalPrefabs[animalIndex].transform.rotation);
         } 
     }
