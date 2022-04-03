697d74ccff305213f9ddd630565f171cd6ec9e03
Lutas <Fanateshen@gmail.com>
2022-04-03 11:50:11 +0300

    Update DestroyOutOfBounds.cs

--- Assets/Scenes/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scenes/Scripts/DestroyOutOfBounds.cs
@@ -9,12 +9,14 @@ public class DestroyOutOfBounds : MonoBehaviour
 
     void Update()
     {
+        // If an object goes past the players view in the game, remove that object
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
         if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
